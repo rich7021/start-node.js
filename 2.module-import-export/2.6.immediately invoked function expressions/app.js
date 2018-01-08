601e2a53e320f52d2527301f561cbc47ea9015c3
rich7021 <coconutfu@gmail.com>
2018-01-08 23:36:10 +0800

    update 2.6

--- 2.module-import-export/2.6.immediately invoked function expressions/app.js
+++ 2.module-import-export/2.6.immediately invoked function expressions/app.js
@@ -2,6 +2,6 @@ var lastname = "Fu";
 (function() {
   var lastname = "Richard";
   console.log(lastname);
-})();
+})(); // the outside parenthesis means it's a expression, and the last parenthesis means invoke it
 
-console.log(lastname);
+console.log(lastname); // prove that the lastname with "Richard" just in this scope
